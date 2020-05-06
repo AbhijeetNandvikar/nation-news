d29fdc5860feae45ccc2c4ece5c51c8557ab411b
AbhijeetNandvikar <47082922+AbhijeetNandvikar@users.noreply.github.com>
2020-05-06 14:04:40 +0530

    Search component added

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,35 +1,57 @@
-import React,{ Component } from 'react' ;
+import React,{ useState } from 'react' ;
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 
-class Search extends Component{
-    render(){
+
+
+const Search = (props) =>{
+			const {
+			  buttonLabel,
+			  className
+			} = props;
+		  
+			const [modal, setModal] = useState(false);
+		  
+			const toggle = () => setModal(!modal);
         return(
-            <div className="search container col-12 col-md-6">
-				<div className="row justify-content-center mt-2">
-					<div className="col-8">
-						<input type="text" class="form-control" placeholder="What you looking?" />
-					</div>
+			<div>
+			 <Button className='sButton'  color='dark' onClick={toggle}><i class="fa fa-search"></i></Button>
+				<Modal isOpen={modal} toggle={toggle} className={className}>
+					<ModalHeader toggle={toggle}>What are you looking for?</ModalHeader>
+					<ModalBody>
+					<div className="search container col-12 col-md-6">
+						<div className="row justify-content-center mt-2">
+							<div className="col-8">
+								<input type="text" class="form-control" placeholder="Search by keyword" />
+							</div>
 
-					<div className="col-4 pl-0">
-						<button type="submit" class="btn btn-block btn-primary">{`Search`}</button>
-					</div>
-				</div>
+							<div className="col-4 pl-0">
+								<button type="submit" class="btn btn-block btn-primary">{`Search`}</button>
+							</div>
+						</div>
 
-				<div className="row justify-content-center mt-2">
-					<div className="col-4">
-					<button className="btn btn-block btn-primary" disabled>{`Categories`}</button>
-					</div>
-					<div className="col-8 pl-0">
-						<div class="form-group  p-0">
-							<select id="inputState" class="form-control">
-								<option selected>Choose...</option>
-								<option>...</option>
-							</select>
+						<div className="row align-items-center justify-content-center mt-2">
+							<div className="col-4 ">
+							<div><h5>Categories:</h5></div>
+							</div>
+							<div className="col-8 pl-0">
+								<div class="form-group  p-0">
+									<select id="inputState" class="form-control">
+										<option selected>Choose...</option>
+										<option>COVID-19</option>
+										<option>Top Headlines</option>
+										<option>Economics</option>
+										<option>Politics</option>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
+					</ModalBody>
+				</Modal>
 				</div>
-	        </div>
+        
         )
-    }
+    
 }
 export default Search ;
